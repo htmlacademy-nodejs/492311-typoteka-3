f71fe90eb89ffaf737a4e08222aa1839d4320f82
alexandr <lexspb19@gmail.com>
2021-01-02 16:58:47 +0300

    color for chalk lib; async/await added

--- src/service/cli/version.js
+++ src/service/cli/version.js
@@ -1,11 +1,12 @@
 'use strict';
-
+const chalk = require(`chalk`);
 const packageJsonFile = require(`../../../package.json`);
+const {MessageColor} = require(`../../utils.js`);
 
 module.exports = {
   name: `--version`,
   run() {
     const version = packageJsonFile.version;
-    console.info(version);
+    console.info(chalk[MessageColor.info](version));
   }
 };
