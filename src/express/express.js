5cf663ddc68a58b97e16c6c155f4a377b0fde854
alexandr <lexspb19@gmail.com>
2021-10-10 22:34:02 +0300

    refactoring & bugfix

--- src/express/express.js
+++ src/express/express.js
@@ -1,36 +1,18 @@
 'use strict';
 
 const express = require(`express`);
-
-const articlesRoutes = require(`./routes/articles-routes`);
-const registerRoutes = require(`./routes/register-routes`);
-const loginRoutes = require(`./routes/login-routes`);
-const searchRoutes = require(`./routes/search-routes`);
-const categoriesRoutes = require(`./routes/categories-routes`);
-const myRoutes = require(`./routes/my-routes`);
-const mainRoutes = require(`./routes/main-routes`);
+const path = require(`path`);
+const routing = require(`./routing/routing`);
 
 const DEFAULT_PORT = 8080;
-
-const app = express();
-const path = require(`path`);
 const PUBLIC_DIR = `public`;
+const app = express();
 
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.set(`view engine`, `pug`);
 app.set(`views`, `${__dirname}/templates`);
 
-app.use(`/articles`, articlesRoutes);
-app.use(`/categories`, categoriesRoutes);
-app.use(`/register`, registerRoutes);
-app.use(`/search`, searchRoutes);
-app.use(`/login`, loginRoutes);
-app.use(`/my`, myRoutes);
-app.use(`/`, mainRoutes);
-app.use((req, res) => {
-  res.status(404);
-  res.render(`404`);
-});
+routing.registerRoutes(app);
 
 app.listen(DEFAULT_PORT);
---
+++ src/express/routing/categories-routes.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const {Router} = require(`express`);
+const categoriesRouter = new Router();
+
+categoriesRouter.get(`/`, (req, res) => {
+  const pageContent = {
+    categories: [
+      {
+        value: 'Жизнь и путешествия'
+      },
+      {
+        value: 'Путешествия'
+      },
+      {
+        value: 'Дизайн и программирование'
+      },
+      {
+        value: 'Другое'
+      },
+      {
+        value: 'Личное'
+      }]
+  };
+  res.render(`categories/categories`, pageContent);
+});
+
+module.exports = categoriesRouter;
