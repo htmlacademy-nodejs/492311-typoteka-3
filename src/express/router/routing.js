9f6e87cc5a8e9a28043f8a3396173aaf87f40b79
alexandr <lexspb19@gmail.com>
2021-10-20 21:36:26 +0300

    naming refactoring

---
+++ src/express/router/routing.js
@@ -0,0 +1,25 @@
+const articlesRoutes = require(`./articles-routes`);
+const registerRoutes = require(`./register-routes`);
+const loginRoutes = require(`./login-routes`);
+const searchRoutes = require(`./search-routes`);
+const categoriesRoutes = require(`./categories-routes`);
+const myRoutes = require(`./my-routes`);
+const mainRoutes = require(`./main-routes`);
+
+function initRoutes(app) {
+  app.use(`/articles`, articlesRoutes);
+  app.use(`/categories`, categoriesRoutes);
+  app.use(`/register`, registerRoutes);
+  app.use(`/search`, searchRoutes);
+  app.use(`/login`, loginRoutes);
+  app.use(`/my`, myRoutes);
+  app.use(`/`, mainRoutes);
+  app.use((req, res) => {
+    res.status(404);
+    res.render(`404`);
+  });
+}
+
+module.exports = {
+  initRoutes
+};
