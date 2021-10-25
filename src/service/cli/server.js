25f22c24876f777ef24fcbd281475b0dca26bcaa
alexandr <lexspb19@gmail.com>
2021-10-25 20:35:08 +0300

    codestyle fixes

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -4,11 +4,9 @@ const express = require(`express`);
 const chalk = require(`chalk`);
 const {MessageColor} = require(`../../utils`);
 const fs = require(`fs`).promises;
-
-const app = express();
-
 const DEFAULT_PORT = 3000;
 const filePath = `mocks.json`;
+const app = express();
 
 const getFileData = async (path) => {
   try {
@@ -31,21 +29,16 @@ const sendResponse = (res, message) => {
   res.send(template);
 };
 
-const onClientConnect = async (res) => {
-  let mocks = await getFileData(filePath);
-  const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-  sendResponse(res, `<ul>${message}</ul>`);
-};
-
 module.exports = {
   name: `--server`,
   async run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
-    app.use(`/posts`, (req, res) => {
-      onClientConnect(res, port);
+    app.use(`/posts`, async (req, res) => {
+      const mocks = await getFileData(filePath);
+      const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
+      sendResponse(res, `<ul>${message}</ul>`);
     });
-
     app.listen(port, () => {
       console.info(chalk[MessageColor.success](`Ожидаю соединений на ${port}`));
     });
